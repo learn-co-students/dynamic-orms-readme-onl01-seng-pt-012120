5e950c982b61e7ef9b2e535d8e72951fb3db9765
Rico Mitchell <ricomitch@icloud.com>
2020-04-07 03:57:39 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 require 'sqlite3'
 
-
+#creates the database
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
-DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB[:conn].execute("DROP TABLE IF EXISTS songs") #dropping songs to avoid
+#an error
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
@@ -13,4 +14,8 @@ sql = <<-SQL
 SQL
 
 DB[:conn].execute(sql)
-DB[:conn].results_as_hash = true
+DB[:conn].results_as_hash = true #results_as_hash is telling us to
+#return a database row as a hash with column names as keys
+
+#returns {"id"=>1, "name"=>"Hello", "album"=>"25", 0 => 1, 1 => "Hello",
+#2 => "25"}
